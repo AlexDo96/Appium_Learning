db62e4f65574c3212e5826aa437be36ab5ad4204
dhtanh <anh.dht3772@gmail.com>
2021-11-06 11:17:26 +0700

    Commit Appium codes 06/11

---
+++ src/Lesson_13/LaunchApp.java
@@ -0,0 +1,43 @@
+package Lesson_13;
+
+import caps.MobileCapabilityTypeEx;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class LaunchApp {
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> appiumDriver = null;
+
+        try {
+            // Setup DesiredCapabilities
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.PLATFORM_NAME, "Android");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.AUTOMATION_NAME, "uiautomator2");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.UDID, "emulator-5554");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.APP_PACKAGE, "com.wdiodemoapp");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.APP_ACTIVITY, "com.wdiodemoapp.MainActivity");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.NEW_COMMAND_TIMEOUT, 120);
+
+            // {capabilities} ----> Appium Server:4273
+            URL appiumServer = new URL("http://localhost:4723/wd/hub");
+            appiumDriver = new AndroidDriver<>(appiumServer, desiredCapabilities);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Interact with Mobile Element
+        if (appiumDriver != null) {
+            appiumDriver.manage().timeouts().implicitlyWait(30L, TimeUnit.SECONDS);
+            System.out.println("Connected to Appium server and launched target app!");
+            MobileElement loginlabel = appiumDriver.findElementByAccessibilityId("Login");
+            loginlabel.click();
+        } else {
+            System.out.println("Error when connecting with Appium server");
+        }
+    }
+}
---
+++ src/Lesson_14/SignupAndLoginApp.java
@@ -0,0 +1,71 @@
+package Lesson_14;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.DriverFactory;
+
+public class SignupAndLoginApp {
+    public static void main(String[] args) {
+        DriverFactory.startAppiumServer();
+        int length = 8;
+        boolean useLetters = true;
+        boolean useNumbersForEmail = false;
+        boolean useNumbersForPassword = true;
+        String generatedStringEmail = RandomStringUtils.random(length, useLetters, useNumbersForEmail);
+        String generatedStringPassword = RandomStringUtils.random(length, useLetters, useNumbersForPassword);
+        String userEmail = generatedStringEmail + "@gmail.com";
+        String userPassword = generatedStringPassword;
+
+        try {
+            AndroidDriver<MobileElement> androidDriver = DriverFactory.getAndroidDriver();
+            // Explicit Wait
+            WebDriverWait explicitWait = new WebDriverWait(androidDriver, 30);
+
+            // 1. Click Login feature
+            MobileElement loginLabel = androidDriver.findElementByAccessibilityId("Login");
+            loginLabel.click();
+
+            // 2. Switch to Sign up tab
+            MobileElement signUpTab = androidDriver.findElementByAccessibilityId("button-sign-up-container");
+            signUpTab.click();
+
+            // 3. Input register info
+            explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.EditText[@content-desc='input-email']"))).sendKeys(userEmail);
+            explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.EditText[@content-desc='input-password']"))).sendKeys(userPassword);
+            explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.EditText[@content-desc='input-repeat-password']"))).sendKeys(userPassword);
+            MobileElement signUpBtn = androidDriver.findElementByAccessibilityId("button-SIGN UP");
+            signUpBtn.click();
+            MobileElement signUpMessage = androidDriver.findElementById("android:id/message");
+            explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.id("android:id/message")));
+            System.out.println("Sign up result: " + signUpMessage.getText());
+            MobileElement okBtn = androidDriver.findElementByXPath("//*[contains(@text, 'OK')]");
+            okBtn.click();
+
+            // 4. Login with registered info
+            MobileElement loginTab = androidDriver.findElementByAccessibilityId("button-login-container");
+            explicitWait.until(ExpectedConditions.elementToBeClickable(loginTab));
+            loginTab.click();
+            MobileElement emailInput = androidDriver.findElementByXPath("//android.widget.EditText[@content-desc='input-email']"); // Relative XPath
+            MobileElement passwordInput = androidDriver.findElementByXPath("//android.widget.EditText[@content-desc='input-password']"); // Relative XPath
+            MobileElement loginBtn = androidDriver.findElementByAccessibilityId("button-LOGIN");
+            MobileElement loginDescription = androidDriver.findElementByXPath("//*[contains(@text, 'When the device')]"); // Conditional XPath
+            emailInput.clear();
+            passwordInput.clear();
+            emailInput.sendKeys(userEmail);
+            passwordInput.sendKeys(userPassword);
+            System.out.println("Login description: " + loginDescription.getText());
+            loginBtn.click();
+            MobileElement loginResultDialog = androidDriver.findElementById("android:id/alertTitle");
+            explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.id("android:id/alertTitle")));
+            System.out.println("Login result: " + loginResultDialog.getText());
+
+        } catch (Exception ignored) {
+        } finally {
+            DriverFactory.stopAppiumServer();
+        }
+    }
+}
